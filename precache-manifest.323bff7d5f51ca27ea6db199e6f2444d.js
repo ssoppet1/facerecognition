1f7fd135feff3b36e6c849df0be7d642c6b03c3f
Stacy Soppet <stacysoppet@Stacys-MacBook-Air.local>
2022-07-12 14:03:46 -0500

    Deploy React app to GitHub Pages

---
+++ precache-manifest.323bff7d5f51ca27ea6db199e6f2444d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d6d8def1e4c89e02754",
+    "url": "/facerecognition/static/css/main.1cef25e5.chunk.css"
+  },
+  {
+    "revision": "1d6d8def1e4c89e02754",
+    "url": "/facerecognition/static/js/main.43bba9a4.chunk.js"
+  },
+  {
+    "revision": "a303b7974f4a0df46154",
+    "url": "/facerecognition/static/js/runtime~main.24208c7a.js"
+  },
+  {
+    "revision": "7a633612f8e1ac278a84",
+    "url": "/facerecognition/static/css/2.2c0f6599.chunk.css"
+  },
+  {
+    "revision": "7a633612f8e1ac278a84",
+    "url": "/facerecognition/static/js/2.7ebb3d42.chunk.js"
+  },
+  {
+    "revision": "57b409c86cfae2784249",
+    "url": "/facerecognition/static/js/3.053b0c51.chunk.js"
+  },
+  {
+    "revision": "958f3b378875ddf6fd73dd6a060e1521",
+    "url": "/facerecognition/static/media/CutiveMono-Regular.958f3b37.woff"
+  },
+  {
+    "revision": "15ce6157d5994c21035cc1046a763645",
+    "url": "/facerecognition/index.html"
+  }
+];
